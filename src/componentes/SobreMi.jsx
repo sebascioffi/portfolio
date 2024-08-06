62d59bbda6849b2dcc48d124f908f09662e809db
sebascioffi <sebacioffi12@gmail.com>
2024-08-06 10:57:13 -0300

    titulo

--- src/componentes/SobreMi.jsx
+++ src/componentes/SobreMi.jsx
@@ -15,7 +15,7 @@ const SobreMi = () => {
         <p>Tengo experiencia trabajando con una variedad de bases de datos tanto <strong className='sebastianstrong'>SQL</strong> como <strong className='sebastianstrong'>NoSQL</strong>, lo que me proporciona flexibilidad para elegir la mejor solución según las necesidades específicas de cada proyecto. Además, tengo la capacidad de desarrollar aplicaciones móviles con <strong className='sebastianstrong'>React Native</strong>, ampliando así mi alcance para ofrecer productos digitales integrales y de alta calidad.</p>
       </div>
       <div className="datos">
-        <p><strong>Título:</strong> <strong className='tecnico'>Técnico Universitario en Desarrollo de Software</strong> en la Universidad Argentina de la Empresa UADE (2 finales restantes)</p>
+        <p><strong>Título:</strong>Graduado de <strong className='tecnico'>Técnico Universitario en Desarrollo de Software</strong> en la Universidad Argentina de la Empresa UADE</p>
         <p className='pemail'><strong>Email:</strong> <a href="mailto:sebacioffi12@gmail.com" target="_blank" rel="noopener noreferrer">sebacioffi12@gmail.com</a></p>
         <p><strong>Edad:</strong> 21</p>
         <p><strong>Soy de:</strong> Buenos Aires, Argentina</p>
